e0a2ff9d254870f3f9cd6fe0efdabb68bf9f576f
Hongyu Guo <g0h3y6@buaa.edu.cn>
2020-10-23 10:53:06 +0800

    对第一个类的初步实现

--- 第五周作业.py
+++ 第五周作业.py
@@ -1,8 +1,45 @@
-class Contact(self,a,b,c):
-    pass
+class Contact:
+    """
+    包括联系人姓名，电话，邮件，头像等属性，以及更新属性，获取属性的方法，类变量为
+    """
+    def __init__(self,name,email,Profile_photo):
+        self.name=name
+        self.email=email
+        self.Profile_photo=Profile_photo
+
+    def update_name(self,name):
+        """
+        更新联系人姓名
+        """
+    def update_email(self,email):
+        """
+        更新联系人电子邮件
+        """
+    def update_Profile_photo(self,Profile_photo):
+        """
+        更新联系人头像
+        """
+    def get_name(self,name):
+        """
+        获取联系人姓名
+        """
+    def get_email(self,email):
+        """
+        获取联系人邮件
+        """
+    def get_Profile_photo(self,Profile_photo):
+        """
+        获取联系人头像
+        """
 class Profile_photo(self,ab,c):
-    pass
+    """
+    包含图片路径，长，宽等属性
+    """
 class Note(self,ab,c):
-    pass
+    """
+    包含联系人数目，联系人列表（包含联系人加入时间，更新时间）等属性，以及加入新联系人（注意重复的电话或邮件不应加入），更新联系人（如果不存在应该提醒），搜索联系人（可按姓名，电话，邮件来分别搜索），联系人排序输出（可按姓名，电话，邮件来进行升降序排列）等行为
+    """
 class Unit_test(self,ab,c):
-    pass
+    """
+    进行测试，建立通信簿并进行各类操作
+    """
